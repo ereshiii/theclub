ac76522395a793a17f72c29d31db811a161fec33
mpoke7552 <80190360+mpoke7552@users.noreply.github.com>
2025-05-23 03:04:18 +0800

    minor changes

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -16,24 +16,35 @@ class SearchController extends Controller
     {
         $query = strtolower($request->input('q'));
 
+        $bannedKeywords = ['spam', 'bao', 'tite', 'inappropriate', 'tanga'];
+
         $results = SearchItem::all()->map(function ($item) use ($query) {
             $score = 0;
-
             if (str_contains(strtolower($item->title), $query))
                 $score += 3;
             if (str_contains(strtolower($item->blurb), $query))
                 $score += 2;
             if (str_contains(strtolower($item->keywords), $query))
                 $score += 5;
-
             $item->score = $score;
             return $item;
-        })->sortByDesc('score')->filter(fn($item) => $item->score > 0);
+        })
+            // Filter out items with banned keywords
+            ->filter(function ($item) use ($bannedKeywords) {
+                $haystack = strtolower($item->title . ' ' . $item->blurb . ' ' . $item->keywords);
+                foreach ($bannedKeywords as $badWord) {
+                    if (str_contains($haystack, $badWord)) {
+                        return false; // Exclude item containing banned word
+                    }
+                }
+                return true;
+            })
+            ->sortByDesc('score')
+            ->filter(fn($item) => $item->score > 0);
 
         return view('search.results', [
             'results' => $results,
             'query' => $query
         ]);
     }
-
 }
